ea6ad5de5a039d0343f606304059607becb9786b
Shuo Yang <imsure95@gmail.com>
2013-11-06 19:25:40 -0600

    Modified upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ./target/neuron_input_parallel_generation-1.0.jar shuo@hc.gps.stthomas.edu:/home/shuo/
+scp ./target/neuron_input_parallel_generation-1.0.jar neuron_meta_partition.py shuo@hc.gps.stthomas.edu:/home/shuo/
