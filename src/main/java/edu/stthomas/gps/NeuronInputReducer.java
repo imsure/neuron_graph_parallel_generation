be3f549958658781600f9fbaf58cf87c34857f41
Shuo Yang <imsure95@gmail.com>
2013-11-06 19:17:36 -0600

    Adding reducer

---
+++ src/main/java/edu/stthomas/gps/NeuronInputReducer.java
@@ -0,0 +1,21 @@
+package edu.stthomas.gps;
+
+import java.io.IOException;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class NeuronInputReducer 
+extends Reducer<IntWritable, MultiWritableWrapper, IntWritable, MultiWritableWrapper> {
+
+	@Override
+	public void reduce(IntWritable key, Iterable<MultiWritableWrapper> values, Context context) 
+			throws IOException, InterruptedException {
+		
+		for (MultiWritableWrapper value : values) {
+			context.write(key, value);
+		}
+	}
+}
